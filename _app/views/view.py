dc3ca8c9b5c6d2d9082179a062265cd56022cb6f
rdralves <rdr.alves@gmail.com>
2024-02-22 14:57:48 -0300

    criação da pasta/módulo controllers e arquivo controller.py para as regras de negócio

--- _app/views/view.py
+++ _app/views/view.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from _app import app
-
+from _app.controllers import *
 @app.route('/')
 def index():
     return render_template('index.html')
